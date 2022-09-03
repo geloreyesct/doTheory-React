e4e9cacf7d4d60d5948ab2c904fbf2840334896a
Angelo Ian Reyes <angelo.ian.reyes@codeandtheory.com>
2022-09-03 17:39:08 +0800

    RCT-102: create a component & use a component with JSX

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const desc =
-  'I just learned how to create a React node and render it into the DOM.';
-const myTitleID = 'main-title';
-const name = 'Gelo';
-
-const header = (
-  <header>
-    <h1 id={myTitleID}>{name}'s First React Element!</h1>
-    <p className='main-desc'>{desc}</p>
-  </header>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className='stats'>Players: 1</span>
+    </header>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(header);
+root.render(<Header />);
