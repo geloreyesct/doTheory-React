bfae79dd59a877cac8162c1c65a5f4a301c9cf97
Angelo Ian Reyes <angelo.ian.reyes@codeandtheory.com>
2022-09-03 17:59:39 +0800

    rct-103: use props to create reusable components

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const Header = (props) => {
   );
 };
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className='player'>
-      <span className='player-name'>Gelo</span>
+      <span className='player-name'>{props.name}</span>
 
-      <Counter />
+      <Counter score={props.score} />
     </div>
   );
 };
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className='counter'>
       <button className='counter-action decrement'> - </button>
-      <span className='counter-score'>35</span>
+      <span className='counter-score'>{props.score}</span>
       <button className='counter-action increment'> + </button>
     </div>
   );
@@ -33,7 +33,10 @@ const App = () => {
       <Header title='Scoreboard' totalPlayers={1} />
 
       {/* Players list */}
-      <Player />
+      <Player name='Gelo' score={50} />
+      <Player name='Treasure' score={90} />
+      <Player name='Ashley' score={85} />
+      <Player name='James' score={80} />
     </div>
   );
 };
