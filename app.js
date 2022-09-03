d9e8e9cc9e22cc3e6a67586b34bd2ff953749a51
Angelo Ian Reyes <angelo.ian.reyes@codeandtheory.com>
2022-09-03 18:29:14 +0800

    RCT-104: create application state

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const players = [
-  {
-    name: 'Gelo',
-    score: 50,
-    id: 1,
-  },
-  {
-    name: 'Treasure',
-    score: 85,
-    id: 2,
-  },
-  {
-    name: 'Ashley',
-    score: 95,
-    id: 3,
-  },
-  {
-    name: 'James',
-    score: 80,
-    id: 4,
-  },
-];
-
 const Header = (props) => {
   return (
     <header>
@@ -78,18 +55,41 @@ class Counter extends React.Component {
   }
 }
 
-const App = (props) => {
-  return (
-    <div className='scoreboard'>
-      <Header title='Scoreboard' totalPlayers={props.initialPlayers.length} />
+class App extends React.Component {
+  state = {
+    players: [
+      {
+        name: 'Gelo',
+        id: 1,
+      },
+      {
+        name: 'Treasure',
+        id: 2,
+      },
+      {
+        name: 'Ashley',
+        id: 3,
+      },
+      {
+        name: 'James',
+        id: 4,
+      },
+    ],
+  };
 
-      {/* Players list */}
-      {props.initialPlayers.map((player) => (
-        <Player name={player.name} key={player.id.toString()} />
-      ))}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className='scoreboard'>
+        <Header title='Scoreboard' totalPlayers={this.state.players.length} />
+
+        {/* Players list */}
+        {this.state.players.map((player) => (
+          <Player name={player.name} key={player.id.toString()} />
+        ))}
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App initialPlayers={players} />);
+root.render(<App />);
